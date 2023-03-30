5fde787289ded3ba43bdff5ef9c9655822fded44
Moeketsi Kotswane <moeketsi.kotswane@net1.com>
2023-03-30 17:22:17 +0200

    icon

--- application/views/login.php
+++ application/views/login.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="<?php echo base_url();?>dist/css/skin-blue.min.css">
     <!-- iCheck -->
     <link rel="stylesheet" href="<?php echo base_url();?>plugins/iCheck/square/blue.css">
-    <link rel="icon" href="<?php echo base_url();?>dist/img/icon.gif">
+    <link rel="icon" href="<?php echo base_url();?>dist/img/icon.png">
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 
 
